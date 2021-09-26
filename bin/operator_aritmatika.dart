55e05df5a83967341085e00803fe8d5347a2b110
askaerlangga <ae.askaerlangga@gmail.com>
2021-09-26 22:13:47 +0700

    operator aritmatika

---
+++ bin/operator_aritmatika.dart
@@ -0,0 +1,26 @@
+void main(){
+  var angka1 = 10;
+  var angka2 = 5;
+
+  //Penjumlahan
+  print(angka1 + angka2);
+
+  //Pengurangan
+  print(angka1 - angka2);
+
+  //Perkalian
+  print(angka1 * angka2);
+
+  //Pembagian
+  print(angka1 / angka2);
+
+  //Pembagian yang dibulatkan
+  print(angka1 ~/ angka2);
+
+  //Modulo atau Sisa Bagi
+  print(angka1 % angka2);
+
+  //Angka negatif (-angka)
+  print(-angka1 + angka2);
+
+}
