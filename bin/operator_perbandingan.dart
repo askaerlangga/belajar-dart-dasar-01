e522b0f2a046f2b431b2d28fb2bbcf1919a5eaf7
askaerlangga <ae.askaerlangga@gmail.com>
2021-09-26 23:21:10 +0700

    operator perbandingan

---
+++ bin/operator_perbandingan.dart
@@ -0,0 +1,24 @@
+void main(){
+  //Menghasilkan Boolean, true atau false
+
+  //Angka
+  var angka1 = 10;
+  var angka2 = 5;
+
+  //Sama dengan
+  print(angka1 == angka2);
+  //Tidak sama dengan
+  print(angka1 != angka2);
+  //Lebih dari
+  print(angka1 > angka2);
+  //Kurang dari
+  print(angka1 < angka2);
+  //Lebih dari sama dengan
+  print(angka1 >= angka2);
+  //Kurang dari sama dengan
+  print(angka1 <= angka2);
+
+  //String
+  print('aska' == 'aska');
+  print('aska' != 'aska');
+}
