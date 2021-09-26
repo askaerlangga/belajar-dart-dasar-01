e086f6ea3c95eb508e53ccc151854ed3598cab06
askaerlangga <ae.askaerlangga@gmail.com>
2021-09-26 21:31:04 +0700

    Tipe data dynamic

---
+++ bin/dynamic.dart
@@ -0,0 +1,14 @@
+void main(){
+  //Tipe data yang bisa berubah-ubah
+  //Integer
+  dynamic variable = 100;
+  print(variable);
+
+  //String
+  variable = 'Ini String';
+  print(variable);
+
+  //Boolean
+  variable = true;
+  print(variable);
+}
