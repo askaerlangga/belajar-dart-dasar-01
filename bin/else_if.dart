95418bf2524d5e9abddbebb2d6cc9ee6271c281d
askaerlangga <ae.askaerlangga@gmail.com>
2021-09-27 22:15:30 +0700

    else if

---
+++ bin/else_if.dart
@@ -0,0 +1,16 @@
+void main(){
+  var nilaiUTS = 80;
+  var nilaiUAS = 50;
+
+  if(nilaiUTS >= 80 && nilaiUAS >= 80){
+    print('A');
+  }else if(nilaiUTS >= 70 && nilaiUAS >= 70){
+    print('B');
+  }else if(nilaiUTS >= 60 && nilaiUAS >= 60){
+    print('c');
+  }else if(nilaiUTS >= 50 && nilaiUAS >= 50){
+    print('D');
+  }else{
+    print('E');
+  }
+}
