a3197cd25b055ee49b2d1727ba453d7946d3907a
askaerlangga <ae.askaerlangga@gmail.com>
2021-09-27 22:28:48 +0700

    null

---
+++ bin/null.dart
@@ -0,0 +1,10 @@
+void main(){
+  //Harus ada nilainya
+  int angka1;
+
+  //Bisa null, dengan menambahkan simbol ? (tanya) di belakang tipe data
+  int? angka2;
+
+  //print(angka1);
+  print(angka2);
+}
