a7dcaf38fb4acaa78757cc8c3985e6bc9c4d0fd1
askaerlangga <ae.askaerlangga@gmail.com>
2021-09-30 22:31:13 +0700

    quiz2

---
+++ bin/quiz2.dart
@@ -0,0 +1,18 @@
+void main(){
+  //Keliling persegi
+  int sisiPersegi = 10;
+  int kelilingPersegi = sisiPersegi * 4;
+  print(kelilingPersegi);
+
+  //Luas lingkaran
+  int jariJariLingkaran = 10;
+  double luasLingkaran = 3.14 * (jariJariLingkaran * jariJariLingkaran);
+  print(luasLingkaran);
+
+  //Volume balok
+  int panjangBalok = 6;
+  int tinggiBalok = 8;
+  int lebarBalok = 10;
+  int volumeBalok = panjangBalok * tinggiBalok * lebarBalok;
+  print(volumeBalok);
+}
